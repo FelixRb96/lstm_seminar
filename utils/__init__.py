cf94d7f093189be102557e531da35a3ec0f3ddc5
Felix Reinbott <Felix.Ka@web.de>
2019-10-30 16:33:51 +0100

    reworking folder structure and getting rid of unneeded files

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from .additional import wait_while_training
---
+++ utils/additional.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+import os
+
+def wait_while_training():
+    """
+    opens netflix in firefox
+    """
+    os.system("firefox https://www.netflix.com/de-en/login")
